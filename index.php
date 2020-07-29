a2c6366ee30a7420d43ad0cf1f125295faa64e3b
Nikita Behmetev <nikitabehmetev@gmail.com>
2020-07-29 09:28:23 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,141 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+//error_reporting(-1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Elasticsearch\ClientBuilder;
+
+$client = ClientBuilder::create()
+    ->setHosts(['localhost:9200']) // указываем, в виде массива, хост и порт сервера elasticsearch
+    ->build();
+
+$start = microtime(true);
+/*
+$backInTime = date('Y-m-d H:i:s');
+$backInTime = date_modify($backInTime, '+15 minutes');
+echo date('Y-m-d H:i:s');
+echo "<br>";
+echo $backInTime;
+*/
+$date = date('Y-m-d H:i:s');
+$backInTime = '-15 minutes';
+
+
+$arFilter = array(
+    "IBLOCK_ID" => 18,
+    "ACTIVE_DATE" => "Y",
+    "ACTIVE" => "Y",
+    "DATE_MODIFY_FROM" => date('Y-m-d H:i:s', strtotime($date . $backInTime)),
+    //"DATE_MODIFY_TO" => date('Y-m-d H:i:s'),
+    "PROPERTY_TOVARMIKROSA_VALUE" => "Да"
+);
+
+$arGroup = array(
+    //"nTopCount" => 50
+);
+
+$arSelect = array(
+    "ID",
+    "DETAIL_PICTURE",
+    "NAME",
+    "IBLOCK_SECTION_ID",
+    "DETAIL_TEXT",
+    "DETAIL_PAGE_URL",
+    "PROPERTY_CML2_ARTICLE",
+    "PROPERTY_TOVAR_MARKETPLEYS",
+    "PROPERTY_TOVARMIKROSA",
+    "PROPERTY_OTOBRAZHAT_OZHIDAEMYY_PRIKHOD",
+    "PROPERTY_MATERIAL"
+);
+
+$res = CIBlockElement::GetList(
+    array(),
+    $arFilter,
+    false,
+    $arGroup,
+    $arSelect
+);
+$num = 0;
+while ($ob = $res->GetNextElement()) {
+    $arFields = $ob->GetFields();
+    echo "<br>" . ++$num;
+    echo "<pre>";
+    print_r($arFields);
+    echo "</pre>";
+
+
+/*
+    $arSelect2 = array("ID", "NAME", "CODE");
+    $res2 = CIBlockElement::GetElementGroups($arFields['ID'], true, $arSelect2);
+    while ($ob = $res2->Fetch()) {
+        print_r($ob);
+    }*/
+}
+
+/*
+echo $arFields["NAME"];
+echo "<br>";
+echo $arFields["ID"];
+echo "<br>";
+echo $arFields["PROPERTY_CML2_ARTICLE_VALUE"];
+echo "<br>";
+echo $arFields["DETAIL_PAGE_URL"];
+echo "<br>";
+echo $arFields["PROPERTY_TOVAR_MARKETPLEYS_VALUE"];
+echo "<br>";
+echo $arFields["PROPERTY_TOVARMIKROSA_VALUE"];
+echo "<br>";
+echo $arFields["PROPERTY_OTOBRAZHAT_OZHIDAEMYY_PRIKHOD_VALUE"];
+echo "<br>";
+echo $arFields["IBLOCK_SECTION_ID"];
+echo "<br>";
+echo CFile::GetPath($arFields["DETAIL_PICTURE"]);
+echo "<br>";
+echo $arFields["DETAIL_TEXT"];
+echo "<br><br>";
+*/
+/*
+$params = [
+    'index' => 'catalog',
+    'type'    => 'item',
+    'id'    => $arFields["ID"],
+    'body'  => [
+        'NAME' => $arFields["NAME"],
+        'ARTICLE' => $arFields["PROPERTY_CML2_ARTICLE_VALUE"],
+        'URL' => $arFields["DETAIL_PAGE_URL"],
+        'DETAIL_PICTURE' => CFile::GetPath($arFields["DETAIL_PICTURE"]),
+        'TOVARMIKROSA' => $arFields["PROPERTY_TOVARMIKROSA_VALUE"],
+        'MARKETPLEYS' => $arFields["PROPERTY_TOVAR_MARKETPLEYS_VALUE"],
+        'OZHIDAEMYY_PRIKHOD' => $arFields["PROPERTY_OTOBRAZHAT_OZHIDAEMYY_PRIKHOD_VALUE"],
+        'DETAIL_TEXT' => $arFields["DETAIL_TEXT"],
+
+        ]
+];*/
+//print_r($params);
+
+//$response = $client->index($params);
+//UPDATE
+/*
+$paramsUp = [
+    'index' => 'catalog',
+    'type'    => 'item',
+    'id'    => $arFields["ID"],
+    'body'  => [
+        'doc' => [
+            'MATERIAL' => $arFields["PROPERTY_MATERIAL_VALUE"]
+
+        ]
+    ]
+];
+*/
+// Update doc at /my_index/_doc/my_id
+//$response = $client->update($paramsUp);
+
+
+//}
+
+$date = date("m.d.y");
+$time = date("H:i:s");
+
+echo $line = $date . " - " . $time . ': Время выполнения скрипта: ' . round(microtime(true) - $start, 4) . ' сек.';
---
+++ search.php
@@ -0,0 +1,176 @@
+<? require __DIR__ . '/vendor/autoload.php';
+
+use Elasticsearch\ClientBuilder;
+
+$client = ClientBuilder::create()
+    ->setHosts(['localhost:9200']) // указываем, в виде массива, хост и порт сервера elasticsearch
+    ->build();
+
+$params = [
+    'size' => 2,
+    //'scroll' => '3s',
+    'index' => 'catalog',
+    'type' => 'item',
+    'body' => [
+        'query' => [
+            'bool' => [
+                'should' => [
+                    ['match' => ['ARTICLE' => switcher_ru($_GET['q'])]],
+                ]
+            ]
+        ]
+    ]
+];
+//todo: добавить похожие товары
+$results = $client->search($params);
+
+if ($results['hits']['total']['value'] == 1) {
+    echo "полное совпадение по артикулу:<br>";
+    echo '<pre>', print_r($results, true), '</pre>';
+} else {
+    echo "не артикул, - ищем дальше";
+    echo "...<br>";
+    $params = [
+        'size' => 5,
+        //'scroll' => '3s',
+        'index' => 'catalog',
+        'type' => 'item',
+        'body' => [
+            'query' => [
+                'bool' => [
+                    'should' => [
+                        ['match' => ['ARTICLE' => switcher_ru($_GET['q'])]],
+                        ['match' => ['NAME' => switcher_ru($_GET['q'])]],
+                    ]
+                ]
+            ]
+        ]
+    ];
+    $results = $client->search($params);
+    if ($results['hits']['total']['value'] < 1) {
+        echo "нет совпадений";
+        $params = [
+            'index' => 'catalog',
+            'size' => 2,
+            'type' => 'item',
+            'body' => [
+                'query' => [
+                    'fuzzy' => [
+                        'ARTICLE' => [
+                            'value' => switcher_ru($_GET['q']),
+                            'fuzziness' => 1,
+                            'transpositions' => false,
+                            "rewrite" => "constant_score",
+                            //'max_expansions' => 50,
+                            //"boost" => 1.0,
+                            //"prefix_length" => 1,
+                            //"max_expansions" => 100
+                        ]
+                    ],
+                ],
+            ]
+
+        ];
+        $results = $client->search($params);
+
+        echo '<pre>', print_r($results, true), '</pre>';
+
+
+    } else {
+        echo "полное совпадение по артикулу + имя:<br>";
+        echo '<pre>', print_r($results, true), '</pre>';
+    }
+}
+
+
+/*
+$params = [
+    'index' => 'catalog',
+    'size' => 20,
+    'type' => 'item',
+    'body' => [
+        'query' => [
+            'fuzzy' => [
+                'NAME' => [
+                    'value' => $_GET['q'],
+                    'fuzziness' => 2,
+                    'transpositions' => false,
+                    "rewrite" => "constant_score",
+                    //'max_expansions' => 50,
+                    //"boost" => 1.0,
+                    //"prefix_length" => 1,
+                    //"max_expansions" => 100
+                ]
+            ],
+        ],
+    ]
+    /*
+    'body' => [
+        'query' => [
+            'bool' => [
+                'filter' => [
+
+                ],
+                'should' => [
+                    ['match' => ['ARTICLE' => $_GET['q']]],
+                    ['match' => ['NAME' => $_GET['q']]],
+                ],
+            ],
+        ],
+    ],
+];
+
+try {
+    $results = $client->search($params);
+} catch (Exception $e) {
+    var_dump($e->getMessage());
+}
+echo '<pre>', print_r($results, true), '</pre>';
+*/
+/*
+
+if ($results['hits']['total']['value'] <= 1) {
+    $params = [
+        'size' => 5,
+        'scroll' => '3s',
+        'index' => 'catalog',
+        'type' => 'item',
+        'body' => [
+            'query' => [
+                'bool' => [
+                    'should' => [
+                        ['match' => ['ARTICLE' => switcher_ru($_GET['q'])]],
+                        ['match' => ['NAME' => switcher_ru($_GET['q'])]],
+                    ]
+                ]
+            ]
+        ]
+    ];
+
+    $results = $client->search($params);
+}
+
+echo '<pre>', print_r($results, true), '</pre>';
+
+*/
+function switcher_ru($value)
+{
+    $converter = array(
+        'f' => 'а', ',' => 'б', 'd' => 'в', 'u' => 'г', 'l' => 'д', 't' => 'е', '`' => 'ё',
+        ';' => 'ж', 'p' => 'з', 'b' => 'и', 'q' => 'й', 'r' => 'к', 'k' => 'л', 'v' => 'м',
+        'y' => 'н', 'j' => 'о', 'g' => 'п', 'h' => 'р', 'c' => 'с', 'n' => 'т', 'e' => 'у',
+        'a' => 'ф', '[' => 'х', 'w' => 'ц', 'x' => 'ч', 'i' => 'ш', 'o' => 'щ', 'm' => 'ь',
+        's' => 'ы', ']' => 'ъ', "'" => "э", '.' => 'ю', 'z' => 'я',
+
+        'F' => 'А', '<' => 'Б', 'D' => 'В', 'U' => 'Г', 'L' => 'Д', 'E' => 'Е', '~' => 'Ё',
+        ':' => 'Ж', 'P' => 'З', 'B' => 'И', 'Q' => 'Й', 'R' => 'К', 'K' => 'Л', 'V' => 'М',
+        'Y' => 'Н', 'J' => 'О', 'G' => 'П', 'H' => 'Р', 'C' => 'С', 'N' => 'Т', 'E' => 'У',
+        'A' => 'Ф', '{' => 'Х', 'W' => 'Ц', 'X' => 'Ч', 'I' => 'Ш', 'O' => 'Щ', 'M' => 'Ь',
+        'S' => 'Ы', '}' => 'Ъ', '"' => 'Э', '>' => 'Ю', 'Z' => 'Я',
+
+        '@' => '"', '#' => '№', '$' => ';', '^' => ':', '&' => '?', '/' => '.', '?' => ',',
+    );
+
+    $value = strtr($value, $converter);
+    return $value;
+}
